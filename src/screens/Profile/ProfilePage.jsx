d3e19d48eb472a980d61544ff2b8db41e9973da7
CelinaPlatt <alvarezdiaz.celina@gmail.com>
2021-11-10 15:31:35 +0000

    Add  profile card styling

--- src/screens/Profile/ProfilePage.jsx
+++ src/screens/Profile/ProfilePage.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { StyleSheet, View, Pressable } from 'react-native';
 import { logoutHandler } from '../../utils/dbInteraction';
 import style from './style';
-import { Button, Text ,Avatar} from 'react-native-paper';
+import { Button, Text, Avatar } from 'react-native-paper';
 import { LinearGradient } from 'expo-linear-gradient';
 import { withTheme } from 'react-native-elements';
 
-const ProfilePage = ({ user, setUser, navigation, setIsLoggedIn, theme }) => {
+const ProfilePage = ({ user, setUser, navigation, setIsLoggedIn }) => {
   const { fullName, displayName, email } = user;
   const postNavigator = () => {
     navigation.navigate('ProfilePosts');
   };
-
-  const { colors } = theme;
 
   return (
     <View style={style.container}>
@@ -20,21 +18,33 @@ const ProfilePage = ({ user, setUser, navigation, setIsLoggedIn, theme }) => {
         colors={['#252525', '#181818']}
         style={style.background}
       />
+      <View style={style.profilecard}>
+        <Avatar.Image
+        style={style.shadow}
+          size={120}
+          source={require('../../../assets/pexels-cottonbro-6503569.jpg')}
+        />
+        <View  style={style.profiledetails}>
+          <Text style={style.username}> {displayName}</Text>
+          <Text> City </Text>
 
-<Avatar.Image size={100} source={require('../../../assets/pexels-cottonbro-6503569.jpg')} />
-      <Text>Display Name: {displayName}</Text>
-      <Text>Email: {email}</Text>
-
+          <Button
+            style={(style.logoutbutton, style.shadow)}
+            theme={{ colors: { primary: '#252525' } }}
+            mode="contained"
+            onPress={() => {
+              logoutHandler(setUser, setIsLoggedIn);
+            }}
+          >
+            Logout
+          </Button>
+        </View>
+      </View>
       <Button
-        style={ style.button}
+        style={(style.button, style.shadow)}
         mode="contained"
-        onPress={() => {
-          logoutHandler(setUser, setIsLoggedIn);
-        }}
+        onPress={postNavigator}
       >
-        Logout
-      </Button>
-      <Button style={ style.button} mode="contained" onPress={postNavigator}>
         Posts
       </Button>
     </View>
--- src/screens/Profile/style.js
+++ src/screens/Profile/style.js
@@ -1,28 +1,63 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
-button: {
-    justifyContent: "center",
+  button: {
+    justifyContent: 'center',
     height: 40,
     marginTop: 50,
     borderRadius: 5,
     paddingLeft: 20,
     paddingRight: 20,
     elevation: 10,
-    width:150,
+    width: 150,
   },
-  
+  logoutbutton: {
+    justifyContent: 'center',
+    height: 40,
+    marginTop: 10,
+    borderRadius: 5,
+    paddingLeft: 20,
+    paddingRight: 20,
+    width: 150,
+  },
+
   background: {
     position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
     height: '100%',
+  },
+
+  gradientbutton: {
+    height: '100%',
+  },
+  profilecard: {
+    // flex: 1,
+    // alignItems: "center",
+    // justifyContent: "flex-start",
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 30,
+  },
+  profiledetails: {
+    marginLeft: 20,
+    alignItems: 'center',
+  },
+  username: {
+    fontSize: 26,
+  },
+  city: {
+    fontSize: 16,
+  },
+
+  shadow: {
+    elevation: 10,
   },
 });
